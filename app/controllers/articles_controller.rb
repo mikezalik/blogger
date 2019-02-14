5fc71cc3d350df11dc0c9fdc909b46080e691ab2
mikezalik <mike.zalik@gmail.com>
2019-02-13 19:03:08 -0800

    article index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
     def new
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -12,6 +20,6 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :text)
   end
-  
+
     end
 end
